448ebfcfe120b0ceb95b71c55a370d79e449fadc
isaias-B-Oliveira <joseisaias123bezerra@gmail.com>
2023-02-28 15:33:58 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function clock(){
+    var h = new Date().getHours();
+    var m = new Date().getMinutes();
+    var s = new Date().getSeconds();
+     
+    var hours = document.getElementById("hour").innerHTML = h;
+    var minutes = document.getElementById("minutes").innerHTML = m;
+    var seconds = document.getElementById("seconds").innerHTML = s;
+}
+var interval = setInterval(clock, 1000);
